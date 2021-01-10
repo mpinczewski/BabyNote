ffb1a2ece14aa7f39ef5676109575e409579e913
Szymon <ubnsensei@gmail.com>
2021-01-10 15:21:02 +0100

    test fix

--- users/views.py
+++ users/views.py
@@ -15,7 +15,6 @@ class LoginView(APIView):
     def get(self, request):
 
         response = {'request': request.data}
-
         return Response(response)
 
     def post(self, request):
@@ -34,7 +33,6 @@ class LoginView(APIView):
             raise exceptions.AuthenticationFailed('Wrong Password.')
 
         token = get_token(user)
-
         return Response({'token': token})
 
 
