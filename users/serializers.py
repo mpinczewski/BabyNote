ca38462422535c59c4d04d6387c04866745fa3df
Szymon <ubnsensei@gmail.com>
2021-02-02 20:09:14 +0100

    baby views

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,7 @@
-from .models import CustomUser, Profile
+from .models import Baby, CustomUser, Profile, Baby
 from rest_framework import serializers
+from .utils.user_authentication import authenticate_user, get_profile, get_profile_id
+
 
 class RegistrationSerializer(serializers.ModelSerializer):
 
@@ -53,3 +55,30 @@ class ProfileSerializer(serializers.HyperlinkedModelSerializer):
             "address",
             "gender"
         ]
+
+class BabySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Baby
+        fields = [
+            "profile",
+            "baby_name",
+            "baby_birth",
+            "baby_gender",
+            "baby_weight",
+            "baby_height",
+        ]
+
+    
+    def create_baby(self):
+
+        baby = Baby(
+            profile = self.validated_data["profile"],
+            baby_name = self.validated_data["baby_name"],
+            baby_birth = self.validated_data["baby_birth"],
+            baby_gender = self.validated_data["baby_gender"],
+            baby_weight = self.validated_data["baby_weight"],
+            baby_height = self.validated_data["baby_height"],
+            )
+
+        baby.save()
+        return baby
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import ProfileDetails, Users, UserDetails, RegisterUser, LoginView
+from .views import BabiesList, BabyDetails, ProfileDetails, Users, UserDetails, RegisterUser, LoginView
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -9,6 +9,10 @@ urlpatterns = [
     path("register/", RegisterUser.as_view(), name="register"),
     path("user_details/", UserDetails.as_view(), name="user_details"),
     path("profile/", ProfileDetails.as_view(), name="profile"),
+
+    path("baby/<pk>", BabyDetails.as_view(), name="baby"),
+    path("baby/list/", BabiesList.as_view(), name="baby_list"),
+
     # restframework_simplejwt
     path("api/token/", TokenObtainPairView.as_view()),
     path("api/token/refresh", TokenObtainPairView.as_view()),
